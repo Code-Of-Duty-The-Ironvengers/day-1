929c03caaee572b0de7aea30a432f71f7e6674db
André de Albuquerque <itstheandre@gmail.com>
2022-05-17 21:30:07 +0200

    Start

---
+++ app.js
@@ -0,0 +1,40 @@
+console.log("I see what you did there");
+
+// const
+// let
+// var
+// variable assigned and declared
+// assignment / initialization =
+// declaration const / let /var
+let simpleVariableForClassPurposes = 0;
+simpleVariableForClassPurposes = simpleVariableForClassPurposes + 1;
+simpleVariableForClassPurposes = 2;
+
+// const newValue = value + 1
+console.log(simpleVariableForClassPurposes + 12);
+
+simpleVariableForClassPurposes++;
+simpleVariableForClassPurposes = simpleVariableForClassPurposes + 2;
+simpleVariableForClassPurposes += 2; // simpleVariable... = simpleVariable...+ 2
+simpleVariableForClassPurposes -= 10;
+simpleVariableForClassPurposes *= 2;
+simpleVariableForClassPurposes /= 5;
+simpleVariableForClassPurposes **= 3;
+
+let name = "Kira";
+let secondName = "Dani";
+
+console.log(
+  "Their names are " +
+    name +
+    " and " +
+    secondName +
+    simpleVariableForClassPurposes
+);
+
+console.log("1" + 1);
+console.log(1 + "1");
+console.log("10" - 1);
+console.log("15" - "Andre");
+console.log(typeof NaN);
+console.log(5 % 2);
